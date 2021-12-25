f44afb8c717a1700ec9d98827e2ba20fc51a336c
tywhisky <mikoto221@qq.com>
2021-12-25 17:17:47 +0800

    feat: add sass and add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../src/pages/Login/index";
-
+import './App.scss'
 // import '../styles/global.css'
 
 // import Layout from '../containers/Layout'
@@ -15,7 +15,7 @@ const App = () => {
     <Router>
       {/* <Layout> */}
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/login" element={<Login/>}/>
           <Route path="/recovery-password" element={<RecoveryPassword/>}/>
           <Route path="*" element={<NotFound/>}/> */}
