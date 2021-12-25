15b9ee659b4024d65eec1cb2b90291c9728cf330
tywhisky <mikoto221@qq.com>
2021-12-25 20:59:41 +0800

    feat: add sider bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Drawn from "../src/components/Drawn/index";
 import Login from "../src/pages/Login/index";
-import './App.scss'
-// import '../styles/global.css'
+import Dashboard from "../src/pages/Dashboard/index";
 
-// import Layout from '../containers/Layout'
-// import Home from '../pages/Home'
-// import Login from '../containers/Login'
-// import RecoveryPassword from '../containers/RecoveryPassword'
-// import NotFound from '../pages/NotFound'
+import "./App.scss";
 
 const App = () => {
   return (
     <Router>
-      {/* <Layout> */}
       <Routes>
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/login" element={<Login/>}/>
-          <Route path="/recovery-password" element={<RecoveryPassword/>}/>
-          <Route path="*" element={<NotFound/>}/> */}
+          <Route path="/" element={<Drawn />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
       </Routes>
-      {/* </Layout> */}
     </Router>
   );
 };
