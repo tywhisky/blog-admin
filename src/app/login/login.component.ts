632727b5d15b68d6824f9e37a00d6a511cd91536
tywhisky <mikoto221@qq.com>
2022-01-26 17:22:42 +0800

    feat: add login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  email: string = ""
+  password: string | undefined
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLogin(): void {
+    alert(this.email)
+  }
 }
