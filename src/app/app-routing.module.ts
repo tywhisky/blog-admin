2b9ad833d49f8ccdba69925146da6abc8367af22
tywhisky <mikoto221@qq.com>
2022-01-29 11:50:04 +0800

    feat: add lint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 import { LayoutComponent } from "./pages/layout/layout.component";
-import {LoginComponent} from "./pages/login/login.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: LayoutComponent,
     children: [
       {
-        path: 'welcome',
-        loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
-      }
-    ]
+        path: "welcome",
+        loadChildren: () =>
+          import("./pages/welcome/welcome.module").then((m) => m.WelcomeModule),
+      },
+    ],
   },
   {
-    path: 'login',
-    component: LoginComponent
-  }
+    path: "login",
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
